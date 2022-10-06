5aaa1ea6f7b3bf6c29fa1e0ef5d645a997597847
SviridovL <sviridovl@gmail.com>
2022-10-06 18:44:52 +0300

    Done

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,4 +1,8 @@
+ const start = Date.now();
+ 
 cookie.onclick = function () {
+  const end = Date.now();
+  
   let cookie = document.getElementById("cookie");
   const clicker__counter = document.getElementById("clicker__counter");
   console.log(clicker__counter.textContent);
@@ -12,3 +16,28 @@ cookie.onclick = function () {
   }
   return;
 };
+
+// cookie.onclick = function () {
+//   let cookie = document.getElementById("cookie");
+//   const clicker__counter = document.getElementById("clicker__counter");
+//   //console.log(clicker__counter.textContent);
+//   const counter = Number(clicker__counter.textContent) + 1;
+//   clicker__counter.textContent = counter;
+//   //console.log(clicker__counter.textContent);
+//   if (cookie.width === 200) {
+//     cookie.width = 100;
+//   } else if (cookie.width === 100) {
+//     cookie.width = 200;
+//   }
+//   speedometer();
+//   speed__click.textContent = speed;
+//   return;
+// };
+// function speedometer() {
+//  
+//   let diff = end - start;
+//   let speed = (counter * 1000) / diff;
+//   speed = speed.toFixed(3);
+//   speed = String(speed);
+//   return speed;
+// }
