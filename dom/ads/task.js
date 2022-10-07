8e88f3c4f995de133dcd3f4348f1b903fcb60c92
SviridovL <sviridovl@gmail.com>
2022-10-07 22:48:48 +0300

    Done with color & speed

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,13 +1,17 @@
 setInterval(rotor, 1000);
 const rotator = document.querySelectorAll(".rotator__case");
 const rotatorArr = Array.from(rotator);
-console.log(rotatorArr);
 let i = 0;
 function rotor() {
   rotator.forEach((element) => {
     element.className = "rotator__case";
   });
   rotatorArr[i].classList.add("rotator__case_active");
+  let color = rotatorArr[i].getAttribute("data-color");
+  let speed = rotatorArr[i].getAttribute("data-speed");
+  console.log(color, speed);
+  rotatorArr[i].style.color = color;
+  rotatorArr[i].style.speed = speed;
   i = i + 1;
   if (i === rotatorArr.length) {
     i = 0;
