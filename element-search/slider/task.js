3da70d56ff03fec49b9a3e3e8e7a9b50c2046a10
SviridovL <sviridovl@gmail.com>
2022-10-07 09:05:32 +0300

    Done with dots. Some code optimization still need

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -6,21 +6,62 @@ arrowMinus.addEventListener("click", minus);
 let i = 0;
 function plus() {
   i = i + 1;
+  if (i >= slide.length) {
+    i = i - slide.length;
+  }
   slide.forEach((elm) => {
     elm.className = "slider__item";
   });
-  slide[i].className = "slider__item slider__item_active";
-  if (i >= slide.length - 1) {
+  dot.forEach((elm) => {
+    elm.className = "slider__dot";
+  });
+  slide[Math.abs(i)].className = "slider__item slider__item_active";
+  dotArr[Math.abs(i)].className = "slider__dot slider__dot_active";
+  if (i >= slide.length) {
     i = i - slide.length;
   }
 }
 function minus() {
   i = i - 1;
+  if (Math.abs(i) >= slide.length) {
+    i = i + slide.length;
+  }
   slide.forEach((elm) => {
     elm.className = "slider__item";
   });
+  dot.forEach((elm) => {
+    elm.className = "slider__dot";
+  });
   slide[Math.abs(i)].className = "slider__item slider__item_active";
-  if (Math.abs(i) >= slide.length - 1) {
+  dotArr[Math.abs(i)].className = "slider__dot slider__dot_active";
+  if (Math.abs(i) >= slide.length) {
     i = i + slide.length;
+  }
+}
+const dot = document.querySelectorAll(".slider__dot");
+const dotArr = Array.from(dot);
+dot.forEach((item) => {
+  item.addEventListener("click", dots);
+});
+function dots(event) {
+  slide.forEach((elm) => {
+    elm.className = "slider__item";
+  });
+  dot.forEach((elm) => {
+    elm.className = "slider__dot";
+  });
+  if (Math.abs(i) === slide.length && i < 0) {
+    i = i + slide.length;
+  } else {
+    i = i - slide.length;
+  }
+  event.currentTarget.className = "slider__dot slider__dot_active";
+  //let index = dotArr.indexOf(event.currentTarget);
+  i = dotArr.indexOf(event.currentTarget);
+  slide[Math.abs(i)].className = "slider__item slider__item_active";
+  if (Math.abs(i) === slide.length && i < 0) {
+    i = i + slide.length;
+  } else {
+    i = i - slide.length;
   }
 }
