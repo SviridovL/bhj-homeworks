a196a03b790906fa5d609246991de91bd44bb26b
SviridovL <sviridovl@gmail.com>
2022-10-07 09:26:55 +0300

    With common function

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,29 +3,54 @@ const arrowMinus = document.querySelector(".slider__arrow_prev");
 const slide = document.querySelectorAll(".slider__item");
 arrowPlus.addEventListener("click", plus);
 arrowMinus.addEventListener("click", minus);
+const dot = document.querySelectorAll(".slider__dot");
+const dotArr = Array.from(dot);
+
+dot.forEach((item) => {
+  item.addEventListener("click", dots);
+});
+
 let i = 0;
+
 function plus() {
   i = i + 1;
-  if (i >= slide.length) {
+  if (Math.abs(i) >= slide.length) {
     i = i - slide.length;
   }
-  slide.forEach((elm) => {
-    elm.className = "slider__item";
-  });
-  dot.forEach((elm) => {
-    elm.className = "slider__dot";
-  });
-  slide[Math.abs(i)].className = "slider__item slider__item_active";
-  dotArr[Math.abs(i)].className = "slider__dot slider__dot_active";
+  common();
   if (i >= slide.length) {
     i = i - slide.length;
   }
 }
+
 function minus() {
   i = i - 1;
   if (Math.abs(i) >= slide.length) {
     i = i + slide.length;
   }
+  common();
+  if (Math.abs(i) >= slide.length) {
+    i = i + slide.length;
+  }
+}
+
+function dots(event) {
+  if (Math.abs(i) === slide.length && i < 0) {
+    i = i + slide.length;
+  } else {
+    i = i - slide.length;
+  }
+  i = dotArr.indexOf(event.currentTarget);
+  common();
+
+  if (Math.abs(i) === slide.length && i < 0) {
+    i = i + slide.length;
+  } else {
+    i = i - slide.length;
+  }
+}
+
+function common() {
   slide.forEach((elm) => {
     elm.className = "slider__item";
   });
@@ -34,34 +59,4 @@ function minus() {
   });
   slide[Math.abs(i)].className = "slider__item slider__item_active";
   dotArr[Math.abs(i)].className = "slider__dot slider__dot_active";
-  if (Math.abs(i) >= slide.length) {
-    i = i + slide.length;
-  }
-}
-const dot = document.querySelectorAll(".slider__dot");
-const dotArr = Array.from(dot);
-dot.forEach((item) => {
-  item.addEventListener("click", dots);
-});
-function dots(event) {
-  slide.forEach((elm) => {
-    elm.className = "slider__item";
-  });
-  dot.forEach((elm) => {
-    elm.className = "slider__dot";
-  });
-  if (Math.abs(i) === slide.length && i < 0) {
-    i = i + slide.length;
-  } else {
-    i = i - slide.length;
-  }
-  event.currentTarget.className = "slider__dot slider__dot_active";
-  //let index = dotArr.indexOf(event.currentTarget);
-  i = dotArr.indexOf(event.currentTarget);
-  slide[Math.abs(i)].className = "slider__item slider__item_active";
-  if (Math.abs(i) === slide.length && i < 0) {
-    i = i + slide.length;
-  } else {
-    i = i - slide.length;
-  }
 }
