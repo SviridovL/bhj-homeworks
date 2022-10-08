7dc2956bba8ef50bb83abc84d3c3868adac7533a
SviridovL <sviridovl@gmail.com>
2022-10-08 18:47:30 +0300

    Fully done!

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,5 +1,6 @@
 const fontBtn = document.querySelectorAll(".font-size");
 const book = document.getElementById("book");
+
 fontBtn.forEach((element) => {
   element.addEventListener("click", fontChange);
 });
@@ -10,23 +11,88 @@ function fontChange(e) {
   });
   const activeElement = e.currentTarget;
   activeElement.classList.add("font-size_active");
-
   const size = activeElement.getAttribute("data-size");
 
   if (size === "big") {
-    book.className = "book";
-    console.log(book.className);
+    // book.className = "book";
+    book.classList.remove("book_fs-small");
     book.classList.add("book_fs-big");
-    console.log(book.className);
   }
   if (size === "small") {
-    book.className = "book";
-    console.log(book.className);
+    // book.className = "book";
+    book.classList.remove("book_fs-big");
     book.classList.add("book_fs-small");
-    console.log(book.className);
   }
   if (!size) {
-    book.className = "book";
-    console.log(book.className);
+    book.classList.remove("book_fs-big");
+    book.classList.remove("book_fs-small");
+  }
+}
+
+let fontColor = document.querySelector(".book__control_color");
+const tmpColor = fontColor.children;
+fontColor = Array.from(tmpColor);
+fontColor.forEach((element) => {
+  if (element.matches("a")) {
+    element.addEventListener("click", textColor);
+  }
+});
+
+function textColor(e) {
+  e.preventDefault();
+  fontColor.forEach((element) => {
+    element.classList.remove("color_active");
+  });
+  activeElement = e.currentTarget;
+  activeElement.classList.add("color_active");
+  const color = activeElement.getAttribute("data-text-color");
+  if (color === "black") {
+    book.classList.remove("book_color-whitesmoke");
+    book.classList.remove("book_color-gray");
+    book.classList.add("book_color-black");
+  }
+  if (color === "whitesmoke") {
+    book.classList.remove("book_color-black");
+    book.classList.remove("book_color-gray");
+    book.classList.add("book_color-whitesmoke");
+  }
+  if (color === "gray") {
+    book.classList.remove("book_color-whitesmoke");
+    book.classList.remove("book_color-black");
+    book.classList.add("book_color-gray");
+  }
+}
+
+let bgColor = document.querySelector(".book__control_background");
+const tmpBgColor = bgColor.children;
+bgColor = Array.from(tmpBgColor);
+bgColor.forEach((element) => {
+  if (element.matches("a")) {
+    element.addEventListener("click", backgroundColor);
+  }
+});
+
+function backgroundColor(e) {
+  e.preventDefault();
+  bgColor.forEach((element) => {
+    element.classList.remove("color_active");
+  });
+  activeElement = e.currentTarget;
+  activeElement.classList.add("color_active");
+  const color = activeElement.getAttribute("data-bg-color");
+  if (color === "black") {
+    book.classList.remove("book_bg-white");
+    book.classList.remove("book_bg-gray");
+    book.classList.add("book_bg-black");
+  }
+  if (color === "white") {
+    book.classList.remove("book_bg-black");
+    book.classList.remove("book_bg-gray");
+    book.classList.add("book_bg-white");
+  }
+  if (color === "gray") {
+    book.classList.remove("book_bg-white");
+    book.classList.remove("book_bg-black");
+    book.classList.add("book_bg-gray");
   }
 }
